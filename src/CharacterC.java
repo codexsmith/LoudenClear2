a2862ab987edd9e365c351323871155776de26ca
Donovan Hatch <donovan.hatch@gmail.com>
2012-11-13 15:23:18 -0500

    lexical parser implemented

--- src/CharacterC.java
+++ src/CharacterC.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 /** Character classes require input that is fully enumerated. [a-z] will not work. you must use [a,b,...,w,x,y,z]
  * 
  * @author nick
@@ -11,25 +13,39 @@ public class CharacterC extends iIdentifier{
 	private ArrayList<String> legal;
 	private String title;
 	
-	public CharacterC(String line){
-		String title, symbols;
+	public CharacterC(String line, HashMap<String, CharacterC> map){
+		String title = "", symbols;
+		legal = new ArrayList<String>();
 		
 		line = line.trim();//removes leading & trailing whitespaces
 		
-		if(line.substring(0, 1).compareTo("$") == 0){ //proper identifier
-			title = line.substring(1, line.indexOf(" "));
-			
-			symbols = line.substring(line.lastIndexOf(" ")+1);
-			
-			symbols.replaceAll("[|]", "");//removes brackets
-			symbols.trim();
-		
-		for (String s : symbols.split(",")){
-			legal.add(s);
+		StringTokenizer tokenizer = new StringTokenizer(line, " ", false);
+		this.title = tokenizer.nextToken();
+		String token = "";
+		String regex = "";
+		ArrayList<String> otherCharLegal = null;
+		while(tokenizer.hasMoreTokens())
+		{
+			token = tokenizer.nextToken();
+			if(token.equals("IN"))
+				continue;
+			else if(token.startsWith("$"))
+			{
+				CharacterC charC = map.get(token);
+				otherCharLegal = charC.getLegal();
+				
+			}
+			else if(token.startsWith("["))
+			{
+				//remove brackets and save token
+				regex = token.substring(1, token.length()-1);
+			}
 		}
 		
-		this.title = title;
-		}
+		if(otherCharLegal != null)
+			legal = (ArrayList<String>) otherCharLegal.clone();
+		
+		parseRegex(regex);
 	}
 
 	@Override
@@ -52,6 +68,50 @@ public class CharacterC extends iIdentifier{
 		return title;
 	}
 	
-	
+	private void parseRegex(String regex) //a-z, ^0, A-Z
+	{
+		int strLen = regex.length();
+		if(regex.startsWith("^")) //not
+		{
+			
+			for(int i = 1; i < strLen; i++)
+			{
+				if(regex.length() > i + 2 && regex.charAt(i+1) == '-') //fits a-z pattern
+				{
+					for(int j = regex.charAt(i); j <= regex.charAt(i+2); j++) //ignore - char
+					{
+						legal.remove(String.valueOf((char)j));
+					}
+					i += 2; //account for extra char's used in pattern.
+				}
+				else //single char
+				{
+					char toRemove = regex.charAt(i);
+					String strToRemove = String.valueOf(toRemove);
+					legal.remove(strToRemove);
+				}
+				
+			}
+		}
+		else // normal regex
+		{
+			for(int i = 0; i < strLen; i++)
+			{
+ 				if(regex.length() > i + 2 && regex.charAt(i+1) == '-') //fits a-z pattern
+				{
+					for(int j = regex.charAt(i); j <= regex.charAt(i+2); j++) //ignore - char
+					{
+						legal.add(String.valueOf((char)j));
+					}
+					i += 2; //account for extra char's used in pattern.
+				}
+				else //single char
+				{
+					legal.add(String.valueOf(regex.charAt((char)i)));
+				}
+				
+			}
+		}
+	}	
 	
 }
