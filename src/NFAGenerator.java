b6d3e38c531ad2bbb0b9748f4eafc3f58193c992
Andrew Kim <andrew@localhost.localdomain>
2012-11-17 18:21:20 -0500

    NFAGen javadoc

--- src/NFAGenerator.java
+++ src/NFAGenerator.java
@@ -340,60 +340,80 @@ public class NFAGenerator {
 		if(DEBUG)System.out.println("char_set_list()");
 		if(char_set()&&char_set_list()){
 			return true;
-		}
+		}//end if
 		if(peekChar()==']'){
 			if(match(']'))
 				return true;
-		}
+		}//end if
 		return false;
-	}
+	}//end char_set_list
 	
+	/**
+	 * Validates a character in a character set
+	 * @return True if a valid character. False otherwise
+	 */
 	private boolean char_set(){
 		if(DEBUG)System.out.println("char_set()");
 		if(isCLS_CHAR(peekChar())){
 			if(match(peekChar())&&char_set_tail())
 				return true;
-		}
+		}//end if
 		return false;
-	}
+	}//end char_set
 	
+	/**
+	 * Allows a chararacter set to have multiple characters
+	 * @return True by default
+	 */
 	private boolean char_set_tail(){
 		if(DEBUG)System.out.println("char_set_tail()");
 		if(index>=regex.length()||peekChar()==')'){
 			return true;
-		}
+		}//end if
 		if(peekChar()=='-'){
 			if(match('-')&&isCLS_CHAR(peekChar())){
 				match(peekChar());
 				return true;
-			}
-		}
+			}//end if
+		}//end if
 		toggleEpsilon = true;
 		return true;
-	}
+	}//end char_set_tail method
 	
+	/**
+	 * Handles the exclude set case
+	 * @return True if a valid exclude set syntax if found. False otherwise
+	 */
 	private boolean exclude_set(){
 		if(DEBUG)System.out.println("exclude_set()");
 		if(peekChar()=='^'){
 			if(match('^')&&char_set()&&match(']')&&match('I')&&match('N')&&exclude_set_tail())
 				return true;
-		}
+		}//end if
 		return false;
-	}
+	}//end exclude_set
 	
+	/**
+	 * The tail part of an exclusive set
+	 * @return True if valid syntax if found. False otherwise.
+	 */
 	private boolean exclude_set_tail(){
 		if(DEBUG)System.out.println("exclude_set_tail()");
 		if(peekChar()=='['){
 			if(match('[')&&char_set()&&match(']'))
 				return true;
-		}
+		}//end if
 		String temp = defined_class();
 		if(temp!=null){
 			return true;
-		}
+		}//end if
 		return false;
-	}
+	}//end exclude_set_tail method
 
+	/**
+	 * Parses a defined class given after a '$' character
+	 * @return name of the CharacterC object
+	 */
 	private String defined_class(){
 		if(DEBUG)System.out.println("define_class()");
 		String token = "";
@@ -403,77 +423,109 @@ public class NFAGenerator {
 			while(isUpper(peekChar())){
 				token+=peekChar();
 				match(peekChar());
-			}
+			}//end while
 			if(DEBUG)System.out.println(token);
 			return token;
-		}
+		}//end if
 		return null;
-	}
+	}//end defined_class method
 	
+	/**
+	 * Peeks at the next character in the regex without consuming it.
+	 * @return Character read from the regular expression
+	 */
 	private char peekChar(){
 		if(index>=regex.length())
 			return '\0';
 		if(DEBUG)System.out.println(regex.charAt(index));
 		return regex.charAt(index);
-	}
+	}//end peekChar method
 	
+	/**
+	 * Same as peekChar except this one consumes the character
+	 * @return Character consumed from the regular expression
+	 */
 	private char getChar(){
 		char result = regex.charAt(index);
 		index++;
 		return result;
-	}
+	}//end getChar method
 	
+	/**
+	 * Checks if the next character is an expected character
+	 * @param c Character expected
+	 * @return True if the expected character and the next character matches.
+	 */
 	private boolean match(char c){
 		if(peekChar()==c){
 			if(DEBUG)System.out.printf("Consumed: %c\n",getChar());
 			else{
 				getChar();
-			}
+			}//end else
 			return true;
-		}
+		}//end if
 		
 		else{
 			System.out.printf("Error: Expected %c but found %c\n", c,peekChar());
 			return false;
-		}
-	}
+		}//end else
+	}//end math method
 	
+	/**
+	 * Checks if a character is an instance of RE_CHAR
+	 * @param c Character to be checked
+	 * @return True of c is an instance of RE_CHAR
+	 */
 	private boolean isRE_CHAR(char c){
 		if(c>=0x20&&c<=0x7E){
 			if(c!='\\'&&c!='*'&&c!='+'&&c!='?'&&c!='|'&&c!='['&&c!=']'&&c!='('&&c!=')'&&c!='.'&&c!='\''&&c!='\"'&&c!='$'&&c!='@'){
 				return true;
-			}
-			else if(c=='\\'){
+			}//end if
+			else if(c=='\\'){//character is escaped
 				match('\\');
 				char t = peekChar();
 				if(t==' '||t=='\\'||t=='*'||t=='+'||t=='?'||t=='|'||t=='['||t==']'||t=='('||t==')'||t=='.'||t=='\''||t=='\"'||t=='$'||t=='@'){
 					return true;
-				}
-			}
-		}
+				}//end else if
+			}//end if
+		}//end if
 		return false;
-	}
+	}//end isRE_CHAR method
 	
+	/**
+	 * Checks if a character is an instance of CLS_CHAR
+	 * @param c Character to be checked
+	 * @return True of c is an instance of CLS_CHAR
+	 */
 	private boolean isCLS_CHAR(char c){
 		if(c>=0x20&&c<=0x7E){
 			if(c!='\\'&&c!='^'&&c!='-'&&c!='['&&c!=']'&&c!='@'){
 				return true;
-			}
+			}//end if
 			else if(c=='\\'){
 				match('\\');
 				char t = peekChar();
 				if(t=='\\'||t=='^'||t=='-'||t=='['||t==']'||t=='@'){
 						return true;
-				}
-			}
-		}
+				}//end if
+			}//end else if
+		}//end if
 		return false;
-	}
+	}//end isCLS_CHAR method
 	
+	/**
+	 * Checks if a character is an upper case character
+	 * @param c Character to be checked
+	 * @return True if uppercase.
+	 */
 	private boolean isUpper(char c){
 		return c>='A'&&c<='Z';
-	}
+	}//end isUpper method
 	
+	/**
+	 * Creates 2 new states in the state table transitioning on a particular key
+	 * @param c Key to transition on.
+	 */
 	private void populate(String c){
 		Map<String,ArrayList<TableRow>> trans = new HashMap<String,ArrayList<TableRow>>();
 		TableRow nextRow = new TableRow(new HashMap<String,ArrayList<TableRow>>(), Integer.toString(entry_ind+1), "Invalid Type");
@@ -482,24 +534,33 @@ public class NFAGenerator {
 		trans.put(c, nfa.getTableRowArray(entry_ind+1));
 		nfa.addState(trans, Integer.toString(entry_ind), "Invalid Type", entry_ind, false);
 		entry_ind+=2;
-	}
-	
+	}//end populate
+
+	/**
+	 * Concatenates two states with an epsilon
+	 * @param x Source state index
+	 * @param y Destination state index
+	 */
 	private void concat(int x, int y){
-//		if(entry_ind>2){
 			if(DEBUG)System.out.println("Concated: "+x+" & "+y);
 			ArrayList<TableRow> curr = nfa.getTableRowArray(x);
 			ArrayList<TableRow> next = nfa.getTableRowArray(y);
 			Map<String,ArrayList<TableRow>> nextStates = curr.get(0).getSuccessorStates();
-			if(nextStates.get("@")!=null){
+			if(nextStates.get("@")!=null){//check if map is null for @
 				nextStates.get("@").add(next.get(0));
-			}
+			}//end if
 			else{
 				nextStates.put("@", next);
-			}
+			}//end else
 			curr.get(0).setSuccessorStates(nextStates);
-//		}
-	}
+	}//end concat method
 	
+	/**
+	 * Unions two states pivoted at a particular state.
+	 * @param epsilon Pivot state
+	 * @param state1 State to get unioned
+	 * @param state2 State to get unioned
+	 */
 	private void union(int epsilon, int state1, int state2){
 		if(DEBUG)System.out.println("United: "+state1+" & "+ state2);
 		populate("@");
@@ -511,17 +572,5 @@ public class NFAGenerator {
 		//merge to epsilon
 		concat(state2-1,entry_ind-2);
 		concat(entry_ind-3,entry_ind-2);
-//		if(first_next.get(0).getSuccessorStates().get("@")==null){
-//			first_next.get(0).getSuccessorStates().put("@",nfa.getTableRowArray(entry_ind-2));
-//		}
-//		else{
-//			first_next.get(0).getSuccessorStates().get("@").add(nfa.getTableRowArray(entry_ind-2).get(0));
-//		}
-//		if(second_next.get(0).getSuccessorStates().get("@")==null){
-//			second_next.get(0).getSuccessorStates().put("@",nfa.getTableRowArray(entry_ind-2));
-//		}
-//		else{
-//			second_next.get(0).getSuccessorStates().get("@").add(nfa.getTableRowArray(entry_ind-2).get(0));
-//		}
-	}
-}
+	}//end union method
+}//end NFAGenerator class
