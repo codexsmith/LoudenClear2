1f94c5ee02640af92f0345548c913fab1137fced
nick <nsc319@gmail.com>
2012-12-05 18:08:31 -0500

    phaseII

--- src/Driver.java
+++ src/Driver.java
@@ -7,7 +7,10 @@ public class Driver {
 	public static void main(String[] args) {
 		for(int i = 1; i<=7; i++){
 			String test_case = Integer.toString(i);
-			Tokenizer t = new Tokenizer("C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\input","C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\spec");
+      Tokenizer t = new Tokenizer("C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\input","C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\spec");
+
+			
+// 			Tokenizer t = new Tokenizer("C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\input","C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\spec");
 			String output = t.parse();
 			System.out.println(output);
 		}
