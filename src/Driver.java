49df851ae918bcd000619aaf8b0adf892a02deeb
Nicholas Smith <nsc319@gmail.com>
2012-12-05 19:34:41 -0500

    simpler, one time test driver

--- src/Driver.java
+++ src/Driver.java
@@ -5,15 +5,15 @@ public class Driver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		for(int i = 1; i<=7; i++){
-			String test_case = Integer.toString(i);
-      Tokenizer t = new Tokenizer("C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\input","C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\spec");
-
-			
+		Tokenizer t = new Tokenizer("sample_input.txt","sample_spec.txt");
+		String output = t.parse();
+		System.out.println(output);
+//		for(int i = 1; i<=7; i++){
+//			String test_case = Integer.toString(i);			
 // 			Tokenizer t = new Tokenizer("C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\input","C:\\Users\\Andrew\\workspace\\Parser\\test_inputs\\"+test_case+"\\spec");
-			String output = t.parse();
-			System.out.println(output);
-		}
+//			String output = t.parse();
+//			System.out.println(output);
+//		}
 	}
 
 }
